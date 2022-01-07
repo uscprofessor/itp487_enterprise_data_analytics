3e8a6d77ded71d1af16723c737febd162d5b2576
Mike Lee <mikelee@usc.edu>
2022-01-07 07:15:04 -0800

    Update itp487helper.py

--- py/itp487helper.py
+++ py/itp487helper.py
@@ -54,7 +54,7 @@ def to_mysql(df, host, port, user, password, database, table, debug = False):
   
   print('INSERTING " + str(rows) + " ROWS INTO: ' + database + '.' + table)
   div, mod = divmod(rows/10,60)
-  print('MAY TAKE UP APPROXIMATELY ' + str(div) + ' M and ' + str(mod) + ' SECONDS')
+  print('MAY TAKE APPROXIMATELY ' + str(div) + ' M and ' + str(mod) + ' SECONDS')
   
   if debug:
     print('INSERT FORMAT STRING: ' + sqlformat)
@@ -73,5 +73,6 @@ def to_mysql(df, host, port, user, password, database, table, debug = False):
 
   end = time.time()
   print("ROWS ADDED: " + str(count))
-  print("ELAPSED TIME: " + str(end-start))
+  div, mod = divmod(rows/10,60)
+  print('ELAPSED TIME: ' + str(div) + ' M and ' + str(mod) + ' SECONDS')
   cursor.close()
